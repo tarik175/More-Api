fd5e0dde2d85bf7d235eeb058a56e1d0a0eff92e
Tarık <ahmettarikzengin2002@gmail.com>
2023-01-01 23:38:46 +0300

    additions

--- src/test/java/test_data/HerokuAppTestData.java
+++ src/test/java/test_data/HerokuAppTestData.java
@@ -24,6 +24,17 @@ public class HerokuAppTestData {
         return expectedDataMap;
     }
 
+    public Map<String,Object> expectedDataSetUp1(String firstname,String lastname,int totalprice,boolean depositpaid,Map<String,String> bookingdates){
+
+        Map<String,Object> expectedDataMap=new HashMap<>();
+        expectedDataMap.put("firstname",firstname);
+        expectedDataMap.put("lastname",lastname);
+        expectedDataMap.put("totalprice",totalprice);
+        expectedDataMap.put("depositpaid",depositpaid);
+        expectedDataMap.put("bookingdates",bookingdates);
+        return expectedDataMap;
+    }
+
 
 
 
